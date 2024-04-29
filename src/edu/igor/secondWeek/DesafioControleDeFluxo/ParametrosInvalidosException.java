975c00899dd247341e4ab55af9ed7b83922b2321
0dayIg0r <igormedeiros.f@gmail.com>
2024-04-29 19:30:14 -0300

    desafio java

---
+++ src/edu/igor/secondWeek/DesafioControleDeFluxo/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package edu.igor.secondWeek.DesafioControleDeFluxo;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
